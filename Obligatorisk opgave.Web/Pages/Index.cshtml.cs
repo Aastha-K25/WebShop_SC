70eb39c4c2f3ab2718b56542014b61c640bb6402
Aastha-K25 <aak001@edu.zealand.dk>
2026-09-02 19:52:17 +0200

    index, navbar

--- Obligatorisk opgave.Web/Pages/Index.cshtml.cs
+++ Obligatorisk opgave.Web/Pages/Index.cshtml.cs
@@ -1,18 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Obligatoris.opgave.Domain.Models;
+
 
 namespace Obligatorisk_opgave.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    //Produkterne, som Index.cshtml skal vise.
+    //IReadOnlyList bruges, fordi Razor-siden kun skal læse produkterne og ikke ændre samlingen.
+    public IReadOnlyList<Product> PopularProducts { get; private set; } 
+    = Array.Empty<Product>();
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
+    // OnGet bliver automatisk kaldt, når browseren sender en GET-request til forsiden.
+    
     public void OnGet()
     {
+        PopularProducts = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "Romantisk buket",
+                BookGenre = "Romance",
+                Price = 449m,
+                Imagepath = "..",
+                ImageDescription = "Røde buket med en romantisk bog",
+                IsPopular = true
+            },
+            new Product
+            {
+                Id = 2,
+                Name = "Krimi buket",
+                BookGenre = "Krimi",
+                Price = 449m,
+                Imagepath = "..",
+                ImageDescription = "Hvide buket med en krimi bog",
+                IsPopular = true
+            },
+            new Product
+            {
+                Id = 3,
+                Name = "Fantasy buket",
+                BookGenre = "Fantasy",
+                Price = 449m,
+                Imagepath = "..",
+                ImageDescription = "Lilla buket med en fantasy bog",
+                IsPopular = true,
+            },
+            new Product
+            {
+                Id = 4,
+                Name = "Biografi buket",
+                BookGenre = "Biografi",
+                Price = 449m,
+                Imagepath = "..",
+                ImageDescription = "Orange buket med en biografi bog",
+                IsPopular = true,
+            }
+        };
     }
 }
--- Obligatorisk.opgave.Domain/Models/Product.cs
+++ Obligatorisk.opgave.Domain/Models/Product.cs
@@ -1,6 +1,14 @@
 namespace Obligatoris.opgave.Domain.Models;
 
+//Denne klasse bruges til produktet altså vores buketer. Den indholder alle de properties. 
+
 public class Product
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string BookGenre { get; set; }
+    public decimal Price { get; set; }
+    public string Imagepath { get; set; }
+    public string ImageDescription { get; set; }
+    public bool IsPopular { get; set; }
 }
