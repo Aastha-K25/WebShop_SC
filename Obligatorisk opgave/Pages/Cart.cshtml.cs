2d20eb6605cce61a430371b16f92e6859c2f6736
HelinSaraBaran <sara501d@edu.zealand.dk>
2026-09-02 17:17:55 +0200

    rettelser

--- Obligatorisk opgave/Pages/Cart.cshtml.cs
+++ Obligatorisk opgave/Pages/Cart.cshtml.cs
@@ -4,8 +4,24 @@ namespace Obligatorisk_opgave.Pages;
 
 public class Cart : PageModel
 {
+    // Midlertidige produktdata til kurven
+    public string ProductName { get; set; } = "Romantisk buket";
+
+    public decimal Price { get; set; } = 449;
+
+    public int Quantity { get; set; } = 1;
+
+    // Fast leveringspris indtil videre
+    public decimal ShippingPrice { get; set; } = 49;
+
+    // Beregner prisen ud fra pris og antal
+    public decimal Subtotal => Price * Quantity;
+
+    // Beregner den samlede pris inkl. levering
+    public decimal TotalPrice => Subtotal + ShippingPrice;
+
+    // Kører når Cart-siden åbnes med en GET request
     public void OnGet()
     {
-        
     }
 }
